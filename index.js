3baba8affbc94ff36d76294f87df0f4cac6bb5aa
ayush8006 <ayushagrawal8006@gmail.com>
2025-01-05 02:11:12 +0530

    adding downloading feature through axios

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { Telegraf } = require('telegraf');
 require('dotenv').config();
+
+const axios =require('axios');
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
 const binarySearchString=
 `const binarySearch = function_search(arr, target) {
     let low = 0, high = arr.length - 1;
@@ -14,6 +18,33 @@ const binarySearchString=
 }`
 ;
 
-bot.start((ctx) => ctx.reply('Welcome to Ayush\'s Tele Bot'));
+try {
 
-bot.command('binarysearch',(ctx)=>ctx.reply())
+    bot.start((ctx) => ctx.reply('Welcome to Ayush\'s Tele Bot'));
+
+    bot.command('binarysearch',(ctx)=>ctx.reply(binarySearchString));
+
+    bot.on('sticker', (ctx) => ctx.reply('👍'));
+    
+    bot.command('rollinghashjava',async(ctx)=>{
+        const response= await axios.get('https://raw.githubusercontent.com/shakeebparwez/CP-Learn-And-Solve/refs/heads/main/PatternStringMatching/PolynomialHash.java');
+        ctx.reply(response.data);
+    })
+    
+    bot.command('quit', async (ctx) => {
+    // Explicit usage
+    await ctx.telegram.leaveChat(ctx.message.chat.id)
+  
+    // Using context shortcut
+    await ctx.leaveChat()
+  })
+
+    bot.launch();
+
+} catch (error) {
+
+    console.log("Unexpected Command");
+    
+}
+
+
